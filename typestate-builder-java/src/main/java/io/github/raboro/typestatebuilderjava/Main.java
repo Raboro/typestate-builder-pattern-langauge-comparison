6034aebe50e57aaef4eb9dddc363438699039b83
Raboro <marius.woerfelreal@gmail.com>
2024-03-13 21:55:45 +0100

    added jupiter test dependency with test

--- typestate-builder-java/src/main/java/io/github/raboro/typestatebuilderjava/Main.java
+++ typestate-builder-java/src/main/java/io/github/raboro/typestatebuilderjava/Main.java
@@ -5,4 +5,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static String hi() {
+        return "hi";
+    }
 }
---
+++ typestate-builder-java/src/test/java/io/github/raboro/typestatebuilderjava/MainTest.java
@@ -0,0 +1,13 @@
+package io.github.raboro.typestatebuilderjava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void testHi() {
+        assertEquals("hi", Main.hi());
+    }
+}
