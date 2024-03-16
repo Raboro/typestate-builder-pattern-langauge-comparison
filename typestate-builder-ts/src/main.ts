7eec2750a0aef4a320b18c67ff1c810f0541fd2a
Raboro <marius.woerfelreal@gmail.com>
2024-03-16 17:02:33 +0100

    created utils

--- typestate-builder-ts/src/main.ts
+++ typestate-builder-ts/src/main.ts
@@ -1,1 +1,4 @@
-console.log("Hello world!");
+import { House } from "./utils";
+
+const house = new House([], [], "Garage", "Basement");
+console.log(house);
---
+++ typestate-builder-ts/src/utils.ts
@@ -0,0 +1,15 @@
+export type Wall = "Wall";
+export type Door = "Door";
+export type Garage = "Garage";
+export type Basement = "Basement";
+export type _Window = "Window";
+
+export class House {
+  constructor(
+    readonly walls: Wall[],
+    readonly doors: Door[],
+    readonly garage?: Garage,
+    readonly basement?: Basement,
+    readonly window?: _Window
+  ) {}
+}
